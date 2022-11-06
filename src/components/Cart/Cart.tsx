a46fcade0aa36c41124dce5b73961cf5bcc08f01
william <william.steinkedemello@gmail.com>
2022-11-06 22:01:46 +0100

    add Cart component

---
+++ src/components/Cart/Cart.tsx
@@ -0,0 +1,31 @@
+import React, { FC } from "react";
+import styles from "./Cart.module.css";
+
+interface CartProps {}
+
+const Cart: FC<CartProps> = () => {
+    const cartitems = (
+        <ul className={styles["cart-items"]}>
+            {[{ id: "c1", name: "Sushi", amount: 2, price: 12.99 }].map(
+                (cartitem) => (
+                    <li>{cartitem.name}</li>
+                )
+            )}
+        </ul>
+    );
+    return (
+        <>
+            {cartitems}
+            <div className={styles.total}>
+                <span>Total Amount</span>
+                <span>35.62</span>
+            </div>
+            <div className={styles.actions}>
+                <button className={styles["button--alt"]}>Close</button>
+                <button className={styles.button}>Order</button>
+            </div>
+        </>
+    );
+};
+
+export default Cart;
