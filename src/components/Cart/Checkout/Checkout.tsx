d62f2cb597bc68517668475a2aead0352efb317c
william <william.steinkedemello@gmail.com>
2022-11-15 22:20:47 +0100

    useInput custom hook implemented in Checkout

--- src/components/Cart/Checkout/Checkout.tsx
+++ src/components/Cart/Checkout/Checkout.tsx
@@ -1,4 +1,5 @@
 import React, { FC, MouseEventHandler } from "react";
+import useInput from "../../../hooks/use-input/use-input";
 import styles from "./Checkout.module.css";
 
 interface CheckoutProps {
@@ -6,16 +7,40 @@ interface CheckoutProps {
 }
 
 const Checkout: FC<CheckoutProps> = (props) => {
+    const isNotEmpty = (value: string) => value.trim() !== "";
+    const isEmail = (value: string) => value.includes("@");
+    const {
+        value: name,
+        isValid: nameIsValid,
+        hasError: nameHasError,
+        valueChangeHandler: nameChangeHandler,
+        inputBlurHandler: nameBlurHandler,
+        reset: resetName,
+    } = useInput(isNotEmpty);
+
+    const formIsValid = nameIsValid;
     const submitHandler = (e: React.SyntheticEvent) => {
         e.preventDefault();
-        
+
+        if (!formIsValid) {
+            return;
+        }
+        console.log("Form has been successfully submitted!");
+        resetName();
     };
     return (
         <form onSubmit={submitHandler}>
             <div>
                 <div className={styles.control}>
                     <label htmlFor="name">Your Name</label>
-                    <input type="text" id="name" />
+                    <input
+                        type="text"
+                        id="name"
+                        value={name}
+                        onChange={nameChangeHandler}
+                        onBlur={nameBlurHandler}
+                    />
+                    {nameHasError && <p>Please enter a valid name.</p>}
                 </div>
                 <div className={styles.control}>
                     <label htmlFor="street">Street</label>
--- src/components/use-input/use-input.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import UseInput from './UseInput';
-
-it('It should mount', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<UseInput />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
