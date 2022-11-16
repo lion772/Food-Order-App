28e8f6fa7a39256907960d0f380589703b3c2d1c
william <william.steinkedemello@gmail.com>
2022-11-16 08:12:22 +0100

    useInput custom hook in Checkout component successfully implemented

--- src/components/Cart/Checkout/Checkout.tsx
+++ src/components/Cart/Checkout/Checkout.tsx
@@ -8,7 +8,6 @@ interface CheckoutProps {
 
 const Checkout: FC<CheckoutProps> = (props) => {
     const isNotEmpty = (value: string) => value.trim() !== "";
-    const isEmail = (value: string) => value.includes("@");
     const {
         value: name,
         isValid: nameIsValid,
@@ -17,21 +16,68 @@ const Checkout: FC<CheckoutProps> = (props) => {
         inputBlurHandler: nameBlurHandler,
         reset: resetName,
     } = useInput(isNotEmpty);
+    const {
+        value: street,
+        isValid: streetIsValid,
+        hasError: streetHasError,
+        valueChangeHandler: streetChangeHandler,
+        inputBlurHandler: streetBlurHandler,
+        reset: resetStreet,
+    } = useInput(isNotEmpty);
+    const {
+        value: postal,
+        isValid: postalIsValid,
+        hasError: postalHasError,
+        valueChangeHandler: postalChangeHandler,
+        inputBlurHandler: postalBlurHandler,
+        reset: resetPostal,
+    } = useInput(isNotEmpty);
+    const {
+        value: city,
+        isValid: cityIsValid,
+        hasError: cityHasError,
+        valueChangeHandler: cityChangeHandler,
+        inputBlurHandler: cityBlurHandler,
+        reset: resetCity,
+    } = useInput(isNotEmpty);
 
-    const formIsValid = nameIsValid;
+    let formIsValid = false;
+
+    if (nameIsValid && streetIsValid && postalIsValid && cityIsValid) {
+        formIsValid = true;
+    }
+
     const submitHandler = (e: React.SyntheticEvent) => {
         e.preventDefault();
 
         if (!formIsValid) {
+            console.log(formIsValid);
             return;
         }
         console.log("Form has been successfully submitted!");
         resetName();
+        resetStreet();
+        resetPostal();
+        resetCity();
     };
+
+    const nameFormControl = nameHasError
+        ? `${styles.control} invalid`
+        : `${styles.control}`;
+    const streetFormControl = streetHasError
+        ? `${styles.control} invalid`
+        : `${styles.control}`;
+    const postalFormControl = postalHasError
+        ? `${styles.control} invalid`
+        : `${styles.control}`;
+    const cityFormControl = cityHasError
+        ? `${styles.control} invalid`
+        : `${styles.control}`;
+
     return (
         <form onSubmit={submitHandler}>
             <div>
-                <div className={styles.control}>
+                <div className={nameFormControl}>
                     <label htmlFor="name">Your Name</label>
                     <input
                         type="text"
@@ -42,23 +88,46 @@ const Checkout: FC<CheckoutProps> = (props) => {
                     />
                     {nameHasError && <p>Please enter a valid name.</p>}
                 </div>
-                <div className={styles.control}>
+                <div className={streetFormControl}>
                     <label htmlFor="street">Street</label>
-                    <input type="text" id="street" />
+                    <input
+                        type="text"
+                        id="street"
+                        value={street}
+                        onChange={streetChangeHandler}
+                        onBlur={streetBlurHandler}
+                    />
+                    {streetHasError && <p>Please enter a valid street.</p>}
                 </div>
-                <div className={styles.control}>
+                <div className={postalFormControl}>
                     <label htmlFor="postal">Postal Code</label>
-                    <input type="text" id="postal" />
+                    <input
+                        type="text"
+                        id="postal"
+                        value={postal}
+                        onChange={postalChangeHandler}
+                        onBlur={postalBlurHandler}
+                    />
+                    {postalHasError && <p>Please enter a valid postal code.</p>}
                 </div>
-                <div className={styles.control}>
+                <div className={cityFormControl}>
                     <label htmlFor="city">City</label>
-                    <input type="text" id="city" />
+                    <input
+                        type="text"
+                        id="city"
+                        value={city}
+                        onChange={cityChangeHandler}
+                        onBlur={cityBlurHandler}
+                    />
+                    {cityHasError && <p>Please enter a valid city.</p>}
                 </div>
             </div>
-            <button>Confirm</button>
-            <button type="button" onClick={props.onCancel}>
-                Cancel
-            </button>
+            <div className={styles.actions}>
+                <button>Confirm</button>
+                <button type="button" onClick={props.onCancel}>
+                    Cancel
+                </button>
+            </div>
         </form>
     );
 };
